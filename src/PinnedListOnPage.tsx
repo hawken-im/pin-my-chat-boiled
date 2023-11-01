6080e1816e3ac2028526be238048d9c22b118216
hawken <askhaokun@gmail.com>
2023-11-02 00:35:27 +0800

    Update PinnedListOnPage.tsx

--- src/PinnedListOnPage.tsx
+++ src/PinnedListOnPage.tsx
@@ -7,6 +7,7 @@ interface Props {
 export default function PinnedList(props: Props) {
   const [openList, setOpenList] = useState(true);
   const [listItems, setListItems] = useState([]);
+  const [hoveredId, setHoveredId] = useState<string | null>(null);
 
   const handleCollapse = () => {
     setOpenList(!openList);
@@ -31,26 +32,32 @@ export default function PinnedList(props: Props) {
       <div
         style={{
           display: "flex",
-          justifyContent: "space-between",
+          justifyContent: "right",
           alignItems: "center",
-          marginBottom: "10px",
+          marginBottom: "8px",
         }}
       >
         <button
           onClick={handleCollapse}
           style={{
-            backgroundColor: "#007BFF",
+            backgroundColor: "#292f38",
             color: "white",
-            padding: "8px 16px",
+            padding: "3px 6px",
+            margin: "0 12px 0 0",
             border: "none",
             borderRadius: "4px",
             cursor: "pointer",
           }}
         >
-          {!openList ? "Expand" : "Collapse"}
+          {!openList ? "Expand 🔽" : "Collapse 🔼"}
         </button>
 
-        <div id="pin-my-chat-readyforpin"></div>
+        <div
+          style={{
+            margin: "0 12px 0 0",
+          }}
+          id="pin-my-chat-readyforpin"
+        ></div>
       </div>
 
       <div id="pin-my-chat-pinnedchats">
@@ -68,11 +75,13 @@ export default function PinnedList(props: Props) {
                 <li
                   key={element.id}
                   style={{
-                    padding: "10px 16px",
+                    padding: "4px 8px",
                     borderBottom: "1px solid #f0f0f0",
                     display: "flex",
                     justifyContent: "space-between",
                   }}
+                  onMouseEnter={() => setHoveredId(element.id)}
+                  onMouseLeave={() => setHoveredId(null)}
                 >
                   <button
                     onClick={() => {
@@ -84,36 +93,42 @@ export default function PinnedList(props: Props) {
                       cursor: "pointer",
                       textDecoration: "none",
                       color: "#007BFF",
-                      fontSize: "16px",
+                      fontSize: "13px",
                       textAlign: "left",
                     }}
                   >
                     {element.title}
                   </button>
 
-                  <button
-                    onClick={() => {
-                      chrome.storage.sync.get("pinnedChats", (data) => {
-                        const pinnedChats = data.pinnedChats || [];
-                        const newPinnedChats = pinnedChats.filter(
-                          (chat) => chat.id !== element.id
-                        );
-                        chrome.storage.sync.set({
-                          pinnedChats: newPinnedChats,
+                  {hoveredId === element.id && (
+                    <button
+                      onClick={() => {
+                        chrome.storage.sync.get("pinnedChats", (data) => {
+                          const pinnedChats = data.pinnedChats || [];
+                          const newPinnedChats = pinnedChats.filter(
+                            (chat) => chat.id !== element.id
+                          );
+                          chrome.storage.sync.set({
+                            pinnedChats: newPinnedChats,
+                          });
+                          setListItems(newPinnedChats);
                         });
-                        setListItems(newPinnedChats);
-                      });
-                    }}
-                    style={{
-                      backgroundColor: "transparent",
-                      border: "none",
-                      cursor: "pointer",
-                      color: "#FF4D4D",
-                      fontSize: "18px",
-                    }}
-                  >
-                    📌
-                  </button>
+                      }}
+                      style={{
+                        border: "none",
+                        height: "20px",
+                        fontSize: "12px",
+                        backgroundColor: "#007BFF",
+                        color: "white",
+                        padding: "2px",
+                        margin: "0 0 0 4px",
+                        borderRadius: "2px",
+                        cursor: "pointer",
+                      }}
+                    >
+                      Unpin
+                    </button>
+                  )}
                 </li>
               );
             })}
